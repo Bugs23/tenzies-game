304bbb1bc7b325b12ca0704f365911a83d433989
Raymond <raymondurrutia2@yahoo.com>
2023-01-03 12:24:03 -0500

    Generate an array of 10 random numbers for the new dice

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,18 @@ import "./style.css"
 import Die from "./Die"
 
 export default function App() {
+    function allNewDice() {
+        // create a new dice array to hold the numbers
+        const newDice = []
+        // loop 10 times
+        for (let i = 0; i < 10; i++) {
+            // push a random number from 1-6 to the new dice array
+        	newDice.push(Math.ceil(Math.random() * 6))
+        }
+        // return newDice
+        return newDice
+    }
+
 	return (
 		<main>
 			<div className="dice-container">
