0dcb9b24d2685687ad8e77f9943a8dd4661ef0da
Raymond <raymondurrutia2@yahoo.com>
2023-01-03 19:14:36 -0500

    Added more dice functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,35 @@ import "./style.css"
 import Die from "./Die"
 
 export default function App() {
+	const [dice, setDice] = React.useState(allNewDice())
+
     function allNewDice() {
         // create a new dice array to hold the numbers
         const newDice = []
         // loop 10 times
         for (let i = 0; i < 10; i++) {
-            // push a random number from 1-6 to the new dice array
-        	newDice.push(Math.ceil(Math.random() * 6))
+            // push object with a value property set to the random number 
+            // and an isHeld property set to false
+        	newDice.push({
+        		value: Math.ceil(Math.random() * 6), 
+        		isHeld: false
+        	})
         }
         // return newDice
         return newDice
     }
 
+    function rollDice() {
+    	setDice(allNewDice())
+    }
+
+    const diceElements = dice.map((die) => <Die value={die.value} />)
+
 	return (
 		<main>
 			<div className="dice-container">
-				<Die value="1"/>
-				<Die value="1"/>
-				<Die value="1"/>
-				<Die value="1"/>
-				<Die value="1"/>
-				<Die value="1"/>
-				<Die value="1"/>
-				<Die value="1"/>
-				<Die value="1"/>
-				<Die value="1"/>
+				{diceElements}
+				<button className="roll-dice" onClick={rollDice}>Roll Dice</button>
 			</div>
 		</main>
 	)
