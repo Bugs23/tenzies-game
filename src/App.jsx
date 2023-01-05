b007c1b537250c2a6b07b398032e8c535d2b8269
Raymond <raymondurrutia2@yahoo.com>
2023-01-05 11:32:59 -0500

    added functionality to change button text to New Game and added confetti component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import "./style.css"
 
 import Die from "./Die"
 import { nanoid } from 'nanoid'
+import Confetti from "react-confetti"
 
 export default function App() {
 	const [dice, setDice] = React.useState(allNewDice())
+
+	const [tenzies, setTenzies] = React.useState(false)
 
 	// helper functiom fpr generating a new die that returns an object with all the dice properties
 	function generateNewDie() {
@@ -15,6 +18,21 @@ export default function App() {
 	        		id: nanoid()
         		}	
 	}
+
+	React.useEffect(() => {
+		// check if every dice is being held
+		const allHeld = dice.every((die) => die.isHeld)
+		// get the number being held in the first dice
+		const firstValue = dice[0].value
+		// check if every dice has the same number
+		const sameNumber = dice.every((die) => die.value === firstValue)
+
+		// if all dice are being held and all dice have the same number change tenzies to true
+		if (allHeld && sameNumber) {
+			setTenzies(true)
+		}
+
+	}, [dice])
 
     function allNewDice() {
         // create a new dice array to hold the numbers
@@ -51,12 +69,13 @@ export default function App() {
 
 	return (
 		<main>
+			{tenzies && <Confetti />}
 			<h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
 			<div className="dice-container">
 				{diceElements}
-				<button className="roll-dice" onClick={rollDice}>Roll Dice</button>
 			</div>
+			<button className="roll-dice" onClick={rollDice}>{tenzies ? "New Game" : "Roll Dice"}</button>
 		</main>
 	)
 }
